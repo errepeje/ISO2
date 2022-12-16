16e14401fa62ad2ec09668c013a3ed5ef101de1a
Fernando <lorentecalvo@gmail.com>
2022-12-16 23:17:00 +0100

    visualizar propuesta arreglado

--- src/main/java/presentacion/PantallaAltaCurso.java
+++ src/main/java/presentacion/PantallaAltaCurso.java
@@ -14,8 +14,6 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
--- src/main/java/presentacion/PantallaVisualizarPropuesta.java
+++ src/main/java/presentacion/PantallaVisualizarPropuesta.java
@@ -25,24 +25,25 @@ import java.util.logging.Logger;
 import javax.swing.JLabel;
 
 public class PantallaVisualizarPropuesta extends JFrame {
-	private JPanel contentPane;
-	private JTextField textFieldDiaInicio;
-	private JTextField textFieldMesInicio;
-	private JTextField textFieldAnyoInicio;
-	private JTextField textFieldDiaFin;
-	private JTextField textFieldMesFin;
-	private JTextField textFieldAnyoFin;
-	private JTextField textFieldTasa;
-	private JTextField textFieldECTS;
+	private JPanel contentPane = new JPanel();
+	private JTextField textFieldNombre = new JTextField();
+	private JTextField textFieldEdicion = new JTextField();
+	private JTextField textFieldTipoCurso = new JTextField();
+	private JTextField textFieldEstadoCurso = new JTextField();
+	private JTextField textFieldDiaInicio = new JTextField();
+	private JTextField textFieldMesInicio = new JTextField();
+	private JTextField textFieldAnyoInicio = new JTextField();
+	private JTextField textFieldDiaFin = new JTextField();
+	private JTextField textFieldMesFin = new JTextField();
+	private JTextField textFieldAnyoFin = new JTextField();
+	private JTextField textFieldTasa = new JTextField();
+	private JTextField textFieldECTS = new JTextField();
+	private JTextField textNombreCentro = new JTextField();
+	private JTextField textDirector = new JTextField();
+	private JTextField textFieldSecretario = new JTextField();
+	
 	private GestorPropuestasCursos GPC = new GestorPropuestasCursos();
 	private CursoPropio cp;
-	private JTextField textFieldNombre;
-	private JTextField textFieldEdicion;
-	private JTextField textFieldTipoCurso;
-	private JTextField textFieldEstadoCurso;
-	private JTextField textNombreCentro;
-	private JTextField textDirector;
-	private JTextField textFieldSecretario;
 	/**
 	 * Launch the application.
 	 */
@@ -63,9 +64,7 @@ public class PantallaVisualizarPropuesta extends JFrame {
 		setTitle("Edición de un curso");
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 455, 597);
-		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane); 
 		contentPane.setLayout(null);
 
@@ -77,183 +76,6 @@ public class PantallaVisualizarPropuesta extends JFrame {
 		JList listCursos = new JList(listModel);
 		listCursos.setBounds(10, 11, 166, 525);
 		contentPane.add(listCursos);
-
-		JLabel lblFechaInicio = new JLabel("Fecha de inicio (dd-mm-yyyy): ");
-		lblFechaInicio.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblFechaInicio.setBounds(186, 199, 279, 19);
-		contentPane.add(lblFechaInicio);
-
-		JLabel lblFechaFin = new JLabel("Fecha de fin (dd-mm-yyyy): ");
-		lblFechaFin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblFechaFin.setBounds(186, 252, 279, 19);
-		contentPane.add(lblFechaFin);
-
-		JLabel lblTasaMatricula = new JLabel("Tasa: ");
-		lblTasaMatricula.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblTasaMatricula.setBounds(186, 307, 279, 19);
-		contentPane.add(lblTasaMatricula);
-
-		JLabel lblEditaLosEcts = new JLabel("ECTS: ");
-		lblEditaLosEcts.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblEditaLosEcts.setBounds(186, 357, 279, 19);
-		contentPane.add(lblEditaLosEcts);
-
-		textFieldDiaInicio = new JTextField();
-		textFieldDiaInicio.setEditable(false);
-		textFieldDiaInicio.setToolTipText("");
-		textFieldDiaInicio.setColumns(10);
-		textFieldDiaInicio.setBounds(186, 221, 66, 20);
-		contentPane.add(textFieldDiaInicio);
-
-		JLabel lblg1 = new JLabel("-");
-		lblg1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblg1.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblg1.setBounds(253, 224, 16, 14);
-		contentPane.add(lblg1);
-
-		textFieldMesInicio = new JTextField();
-		textFieldMesInicio.setEditable(false);
-		textFieldMesInicio.setColumns(10);
-		textFieldMesInicio.setBounds(273, 221, 66, 20);
-		contentPane.add(textFieldMesInicio);
-
-		JLabel lblg2 = new JLabel("-");
-		lblg2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblg2.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblg2.setBounds(338, 221, 22, 20);
-		contentPane.add(lblg2);
-
-		textFieldAnyoInicio = new JTextField();
-		textFieldAnyoInicio.setEditable(false);
-		textFieldAnyoInicio.setColumns(10);
-		textFieldAnyoInicio.setBounds(359, 220, 66, 20);
-		contentPane.add(textFieldAnyoInicio);
-
-		textFieldDiaFin = new JTextField();
-		textFieldDiaFin.setEditable(false);
-		textFieldDiaFin.setToolTipText("");
-		textFieldDiaFin.setColumns(10);
-		textFieldDiaFin.setBounds(186, 276, 66, 20);
-		contentPane.add(textFieldDiaFin);
-
-		JLabel lblg11 = new JLabel("-");
-		lblg11.setHorizontalAlignment(SwingConstants.CENTER);
-		lblg11.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblg11.setBounds(253, 279, 16, 14);
-		contentPane.add(lblg11);
-
-		textFieldMesFin = new JTextField();
-		textFieldMesFin.setEditable(false);
-		textFieldMesFin.setColumns(10);
-		textFieldMesFin.setBounds(273, 276, 66, 20);
-		contentPane.add(textFieldMesFin);
-
-		JLabel lblg21 = new JLabel("-");
-		lblg21.setHorizontalAlignment(SwingConstants.CENTER);
-		lblg21.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblg21.setBounds(338, 276, 22, 20);
-		contentPane.add(lblg21);
-
-		textFieldAnyoFin = new JTextField();
-		textFieldAnyoFin.setEditable(false);
-		textFieldAnyoFin.setColumns(10);
-		textFieldAnyoFin.setBounds(359, 275, 66, 20);
-		contentPane.add(textFieldAnyoFin);
-
-		textFieldTasa = new JTextField();
-		textFieldTasa.setEditable(false);
-		textFieldTasa.setBounds(186, 326, 239, 20);
-		contentPane.add(textFieldTasa);
-		textFieldTasa.setColumns(10);
-
-		textFieldECTS = new JTextField();
-		textFieldECTS.setEditable(false);
-		textFieldECTS.setColumns(10);
-		textFieldECTS.setBounds(186, 377, 239, 20);
-		contentPane.add(textFieldECTS);
-
-		JLabel lblNombre = new JLabel("Nombre:");
-		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNombre.setBounds(186, 11, 279, 19);
-		contentPane.add(lblNombre);
-
-		textFieldNombre = new JTextField();
-		textFieldNombre.setToolTipText("");
-		textFieldNombre.setEditable(false);
-		textFieldNombre.setColumns(10);
-		textFieldNombre.setBounds(186, 29, 239, 20);
-		contentPane.add(textFieldNombre);
-
-		textFieldEdicion = new JTextField();
-		textFieldEdicion.setToolTipText("");
-		textFieldEdicion.setEditable(false);
-		textFieldEdicion.setColumns(10);
-		textFieldEdicion.setBounds(186, 82, 239, 20);
-		contentPane.add(textFieldEdicion);
-
-		JLabel lblEdicion = new JLabel("Edicion:");
-		lblEdicion.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblEdicion.setBounds(186, 60, 279, 19);
-		contentPane.add(lblEdicion);
-
-		JLabel lblTipoDeCurso = new JLabel("Tipo de curso:");
-		lblTipoDeCurso.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblTipoDeCurso.setBounds(186, 115, 279, 19);
-		contentPane.add(lblTipoDeCurso);
-
-		textFieldTipoCurso = new JTextField();
-		textFieldTipoCurso.setToolTipText("");
-		textFieldTipoCurso.setEditable(false);
-		textFieldTipoCurso.setColumns(10);
-		textFieldTipoCurso.setBounds(186, 134, 239, 20);
-		contentPane.add(textFieldTipoCurso);
-
-		JLabel lblEstadoCurso = new JLabel("Estado del curso:");
-		lblEstadoCurso.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblEstadoCurso.setBounds(185, 160, 279, 19);
-		contentPane.add(lblEstadoCurso);
-
-		textFieldEstadoCurso = new JTextField();
-		textFieldEstadoCurso.setToolTipText("");
-		textFieldEstadoCurso.setEditable(false);
-		textFieldEstadoCurso.setColumns(10);
-		textFieldEstadoCurso.setBounds(185, 178, 239, 20);
-		contentPane.add(textFieldEstadoCurso);
-
-		JLabel lblNombreCentro = new JLabel("Nombre del centro:");
-		lblNombreCentro.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNombreCentro.setBounds(185, 402, 279, 19);
-		contentPane.add(lblNombreCentro);
-
-		textNombreCentro = new JTextField();
-		textNombreCentro.setEditable(false);
-		textNombreCentro.setColumns(10);
-		textNombreCentro.setBounds(188, 424, 239, 20);
-		contentPane.add(textNombreCentro);
-
-		JLabel lblDirector = new JLabel("Director:");
-		lblDirector.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblDirector.setBounds(184, 452, 279, 19);
-		contentPane.add(lblDirector);
-
-		textDirector = new JTextField();
-		textDirector.setEditable(false);
-		textDirector.setColumns(10);
-		textDirector.setBounds(186, 473, 239, 20);
-		contentPane.add(textDirector);
-
-		JLabel lblSecretario = new JLabel("Secretario:");
-		lblSecretario.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblSecretario.setBounds(181, 497, 279, 19);
-		contentPane.add(lblSecretario);
-
-		textFieldSecretario = new JTextField();
-		textFieldSecretario.setEditable(false);
-		textFieldSecretario.setColumns(10);
-		textFieldSecretario.setBounds(186, 516, 239, 20);
-		contentPane.add(textFieldSecretario);
-
-		/* ACCIONES */
 		listCursos.addListSelectionListener(new ListSelectionListener() {
 			@Override
 			public void valueChanged(ListSelectionEvent e) {
@@ -285,6 +107,150 @@ public class PantallaVisualizarPropuesta extends JFrame {
 				}
 			}
 		});
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNombre.setBounds(186, 11, 279, 19);
+		contentPane.add(lblNombre);
+		textFieldNombre.setToolTipText("");
+		textFieldNombre.setEditable(false);
+		textFieldNombre.setColumns(10);
+		textFieldNombre.setBounds(186, 29, 239, 20);
+		contentPane.add(textFieldNombre);
+		
+		JLabel lblEdicion = new JLabel("Edicion:");
+		lblEdicion.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblEdicion.setBounds(186, 60, 279, 19);
+		contentPane.add(lblEdicion);
+		textFieldEdicion.setToolTipText("");
+		textFieldEdicion.setEditable(false);
+		textFieldEdicion.setColumns(10);
+		textFieldEdicion.setBounds(186, 82, 239, 20);
+		contentPane.add(textFieldEdicion);
+
+		JLabel lblTipoDeCurso = new JLabel("Tipo de curso:");
+		lblTipoDeCurso.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblTipoDeCurso.setBounds(186, 115, 279, 19);
+		contentPane.add(lblTipoDeCurso);
+		textFieldTipoCurso.setToolTipText("");
+		textFieldTipoCurso.setEditable(false);
+		textFieldTipoCurso.setColumns(10);
+		textFieldTipoCurso.setBounds(186, 134, 239, 20);
+		contentPane.add(textFieldTipoCurso);
+		
+		JLabel lblEstadoCurso = new JLabel("Estado del curso:");
+		lblEstadoCurso.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblEstadoCurso.setBounds(185, 160, 279, 19);
+		contentPane.add(lblEstadoCurso);
+		textFieldEstadoCurso.setToolTipText("");
+		textFieldEstadoCurso.setEditable(false);
+		textFieldEstadoCurso.setColumns(10);
+		textFieldEstadoCurso.setBounds(185, 178, 239, 20);
+		contentPane.add(textFieldEstadoCurso);
+		
+		JLabel lblFechaInicio = new JLabel("Fecha de inicio (dd-mm-yyyy): ");
+		lblFechaInicio.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblFechaInicio.setBounds(186, 199, 279, 19);
+		contentPane.add(lblFechaInicio);
+		textFieldDiaInicio.setEditable(false);
+		textFieldDiaInicio.setToolTipText("");
+		textFieldDiaInicio.setColumns(10);
+		textFieldDiaInicio.setBounds(186, 221, 66, 20);
+		contentPane.add(textFieldDiaInicio);
+		JLabel lblg1 = new JLabel("-");
+		lblg1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblg1.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblg1.setBounds(253, 224, 16, 14);
+		contentPane.add(lblg1);
+		textFieldMesInicio.setEditable(false);
+		textFieldMesInicio.setColumns(10);
+		textFieldMesInicio.setBounds(273, 221, 66, 20);
+		contentPane.add(textFieldMesInicio);
+		JLabel lblg2 = new JLabel("-");
+		lblg2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblg2.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblg2.setBounds(338, 221, 22, 20);
+		contentPane.add(lblg2);
+		textFieldAnyoInicio.setEditable(false);
+		textFieldAnyoInicio.setColumns(10);
+		textFieldAnyoInicio.setBounds(359, 220, 66, 20);
+		contentPane.add(textFieldAnyoInicio);
+
+		JLabel lblFechaFin = new JLabel("Fecha de fin (dd-mm-yyyy): ");
+		lblFechaFin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblFechaFin.setBounds(186, 252, 279, 19);
+		contentPane.add(lblFechaFin);
+		textFieldDiaFin.setEditable(false);
+		textFieldDiaFin.setToolTipText("");
+		textFieldDiaFin.setColumns(10);
+		textFieldDiaFin.setBounds(186, 276, 66, 20);
+		contentPane.add(textFieldDiaFin);
+		JLabel lblg11 = new JLabel("-");
+		lblg11.setHorizontalAlignment(SwingConstants.CENTER);
+		lblg11.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblg11.setBounds(253, 279, 16, 14);
+		contentPane.add(lblg11);
+		textFieldMesFin = new JTextField();
+		textFieldMesFin.setEditable(false);
+		textFieldMesFin.setColumns(10);
+		textFieldMesFin.setBounds(273, 276, 66, 20);
+		contentPane.add(textFieldMesFin);
+		JLabel lblg21 = new JLabel("-");
+		lblg21.setHorizontalAlignment(SwingConstants.CENTER);
+		lblg21.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblg21.setBounds(338, 276, 22, 20);
+		contentPane.add(lblg21);
+		textFieldAnyoFin.setEditable(false);
+		textFieldAnyoFin.setColumns(10);
+		textFieldAnyoFin.setBounds(359, 275, 66, 20);
+		contentPane.add(textFieldAnyoFin);
+
+		JLabel lblTasaMatricula = new JLabel("Tasa: ");
+		lblTasaMatricula.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblTasaMatricula.setBounds(186, 307, 279, 19);
+		contentPane.add(lblTasaMatricula);
+		textFieldTasa = new JTextField();
+		textFieldTasa.setEditable(false);
+		textFieldTasa.setBounds(186, 326, 239, 20);
+		contentPane.add(textFieldTasa);
+		textFieldTasa.setColumns(10);
+
+		JLabel lblEditaLosEcts = new JLabel("ECTS: ");
+		lblEditaLosEcts.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblEditaLosEcts.setBounds(186, 357, 279, 19);
+		contentPane.add(lblEditaLosEcts);
+
+		textFieldECTS = new JTextField();
+		textFieldECTS.setEditable(false);
+		textFieldECTS.setColumns(10);
+		textFieldECTS.setBounds(186, 377, 239, 20);
+		contentPane.add(textFieldECTS);
+
+		JLabel lblNombreCentro = new JLabel("Nombre del centro:");
+		lblNombreCentro.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNombreCentro.setBounds(185, 402, 279, 19);
+		contentPane.add(lblNombreCentro);
+		textNombreCentro.setEditable(false);
+		textNombreCentro.setColumns(10);
+		textNombreCentro.setBounds(188, 424, 239, 20);
+		contentPane.add(textNombreCentro);
+
+		JLabel lblDirector = new JLabel("Director:");
+		lblDirector.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblDirector.setBounds(184, 452, 279, 19);
+		contentPane.add(lblDirector);
+		textDirector.setEditable(false);
+		textDirector.setColumns(10);
+		textDirector.setBounds(186, 473, 239, 20);
+		contentPane.add(textDirector);
+		JLabel lblSecretario = new JLabel("Secretario:");
+		lblSecretario.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblSecretario.setBounds(181, 497, 279, 19);
+		contentPane.add(lblSecretario);
+		textFieldSecretario.setEditable(false);
+		textFieldSecretario.setColumns(10);
+		textFieldSecretario.setBounds(186, 516, 239, 20);
+		contentPane.add(textFieldSecretario);
 	}
 	
 	public Date darFormatoFecha(String cadena) {
@@ -296,7 +262,6 @@ public class PantallaVisualizarPropuesta extends JFrame {
 			Logger logger = Logger.getLogger(GestorPropuestasCursos.class.getName());
 			logger.log(null, "Error Fecha");
 		}
-
 		return fecha;
 	}
 }
