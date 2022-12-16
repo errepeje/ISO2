6fd6aef04a014708ee0574b886d9431c1cffa7bf
Fernando <lorentecalvo@gmail.com>
2022-12-17 00:10:42 +0100

    ingresos error calculo

--- src/main/java/presentacion/PantallaConsultarCursos_Ingresos.java
+++ src/main/java/presentacion/PantallaConsultarCursos_Ingresos.java
@@ -12,6 +12,7 @@ import javax.swing.event.ListSelectionListener;
 import controllers.GestorConsultas;
 import controllers.GestorPropuestasCursos;
 import entities.CursoPropio;
+import entities.TipoCurso;
 
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -29,24 +30,23 @@ import java.util.logging.Logger;
 
 import javax.swing.JLabel;
 
-
 public class PantallaConsultarCursos_Ingresos extends JFrame {
-	private JTextField txtIngresos;
-	private JTextField textFieldNombre;
-	private JTextField textFieldEdicion;
-	private JTextField textFieldTipoCurso;
-	private JTextField textFieldEstadoCurso;
-	private JTextField textFieldDiaInicio;
-	private JTextField textFieldMesInicio;
-	private JTextField textFieldAnyoInicio;
-	private JTextField textFieldDiaFin;
-	private JTextField textFieldMesFin;
-	private JTextField textFieldAnyoFin;
-	private JTextField textFieldTasa;
-	private JTextField textFieldECTS;
-	private JTextField textNombreCentro;
-	private JTextField textDirector;
-	private JTextField textFieldSecretario;
+	private JTextField txtIngresos = new JTextField();
+	private JTextField textFieldNombre = new JTextField();
+	private JTextField textFieldEdicion = new JTextField();
+	private JTextField textFieldTipoCurso = new JTextField();
+	private JTextField textFieldEstadoCurso = new JTextField();
+	private JTextField textFieldDiaInicio = new JTextField();
+	private JTextField textFieldMesInicio = new JTextField();
+	private JTextField textFieldAnyoInicio = new JTextField();
+	private JTextField textFieldDiaFin = new JTextField();
+	private JTextField textFieldMesFin = new JTextField();
+	private JTextField textFieldAnyoFin = new JTextField();
+	private JTextField textFieldTasa = new JTextField();
+	private JTextField textFieldECTS = new JTextField();
+	private JTextField textNombreCentro = new JTextField();
+	private JTextField textDirector = new JTextField();
+	private JTextField textFieldSecretario = new JTextField();
 	
 	private GestorConsultas GC = new GestorConsultas();
 	private CursoPropio cp;
@@ -69,20 +69,17 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(panel); 
 		panel.setLayout(null);
-		DefaultListModel listModel = new DefaultListModel();
 		
-		JComboBox tiposBox = new JComboBox();
+		JComboBox tiposBox = new JComboBox(TipoCurso.values());
 		tiposBox.setBounds(10, 28, 166, 21);
 		panel.add(tiposBox);
-		String [] allEstados = {"NINGUN CURSO", "MASTER", "EXPERTO", "ESPECIALISTA", "FORMACION_AVANZADA", "FORMACION_CONTINUA", "MICROCREDENCIALES", "CORTA_DURACION","VERANO_Y_EXTENSION"};
-		for(int i=0; i<allEstados.length; i++)
-			tiposBox.addItem(allEstados[i]);
 		
 		JLabel lblTipoDeCurso1 = new JLabel("Tipo de curso:");
 		lblTipoDeCurso1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblTipoDeCurso1.setBounds(10, 11, 279, 19);
 		panel.add(lblTipoDeCurso1);
 		
+		DefaultListModel listModel = new DefaultListModel();
 		JList listCursos = new JList(listModel);
 		listCursos.setBounds(10, 109, 166, 427);
 		panel.add(listCursos);
@@ -91,29 +88,29 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 			public void valueChanged(ListSelectionEvent e) {
 				if (listCursos.getSelectedValue() != null) {
 					cp = GC.obtenerCursos(Integer.parseInt(listCursos.getSelectedValue().toString()));
-					textFieldTasa.setText(Integer.toString(cp.gettasaMatricula()));
-					textFieldECTS.setText(Integer.toString(cp.geteCTS()));
-
-					String[] resultInicio = cp.getfechaInicio().toString().split("-");
-					String[] solI = resultInicio[2].split(" ");
-					String[] resultFin = cp.getfechaFin().toString().split("-");
-					String[] solF = resultFin[2].split(" ");
 
 					textFieldNombre.setText(cp.getnombre());
 					textFieldEdicion.setText(Integer.toString(cp.getedicion()));
-					textNombreCentro.setText(cp.getNombreCentro());
-					textDirector.setText(cp.getIdDirector());
-					textFieldSecretario.setText(cp.idSecretario);
 					textFieldTipoCurso.setText(cp.gettipo().toString());
 					textFieldEstadoCurso.setText(cp.getestado().toString());
-
+					
+					String[] resultInicio = cp.getfechaInicio().toString().split("-");
+					String[] solI = resultInicio[2].split(" ");
 					textFieldDiaInicio.setText(solI[0]);
 					textFieldMesInicio.setText(Integer.toString(cp.getfechaInicio().getMonth() + 1));
 					textFieldAnyoInicio.setText(Integer.toString(cp.getfechaInicio().getYear() + 1900));
-
+					
+					String[] resultFin = cp.getfechaFin().toString().split("-");
+					String[] solF = resultFin[2].split(" ");
 					textFieldDiaFin.setText(solF[0]);
 					textFieldMesFin.setText(Integer.toString(cp.getfechaFin().getMonth() + 1));
 					textFieldAnyoFin.setText(Integer.toString(cp.getfechaFin().getYear() + 1900));
+					
+					textFieldTasa.setText(Integer.toString(cp.gettasaMatricula()));
+					textFieldECTS.setText(Integer.toString(cp.geteCTS()));
+					textNombreCentro.setText(cp.getNombreCentro());
+					textDirector.setText(cp.getIdDirector());
+					textFieldSecretario.setText(cp.idSecretario);
 				}
 			}
 		});
@@ -122,7 +119,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNombre.setBounds(186, 11, 279, 19);
 		panel.add(lblNombre);
-		textFieldNombre = new JTextField();
 		textFieldNombre.setToolTipText("");
 		textFieldNombre.setEditable(false);
 		textFieldNombre.setColumns(10);
@@ -133,7 +129,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblEdicion.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblEdicion.setBounds(186, 60, 279, 19);
 		panel.add(lblEdicion);
-		textFieldEdicion = new JTextField();
 		textFieldEdicion.setToolTipText("");
 		textFieldEdicion.setEditable(false);
 		textFieldEdicion.setColumns(10);
@@ -144,7 +139,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblTipoDeCurso.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblTipoDeCurso.setBounds(186, 115, 279, 19);
 		panel.add(lblTipoDeCurso);
-		textFieldTipoCurso = new JTextField();
 		textFieldTipoCurso.setToolTipText("");
 		textFieldTipoCurso.setEditable(false);
 		textFieldTipoCurso.setColumns(10);
@@ -155,7 +149,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblEstadoCurso.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblEstadoCurso.setBounds(185, 160, 279, 19);
 		panel.add(lblEstadoCurso);
-		textFieldEstadoCurso = new JTextField();
 		textFieldEstadoCurso.setToolTipText("");
 		textFieldEstadoCurso.setEditable(false);
 		textFieldEstadoCurso.setColumns(10);
@@ -166,7 +159,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblFechaInicio.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblFechaInicio.setBounds(186, 199, 279, 19);
 		panel.add(lblFechaInicio);
-		textFieldDiaInicio = new JTextField();
 		textFieldDiaInicio.setEditable(false);
 		textFieldDiaInicio.setToolTipText("");
 		textFieldDiaInicio.setColumns(10);
@@ -178,7 +170,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblg1.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		lblg1.setBounds(253, 224, 16, 14);
 		panel.add(lblg1);
-		textFieldMesInicio = new JTextField();
 		textFieldMesInicio.setEditable(false);
 		textFieldMesInicio.setColumns(10);
 		textFieldMesInicio.setBounds(273, 221, 66, 20);
@@ -188,7 +179,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblg2.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		lblg2.setBounds(338, 221, 22, 20);
 		panel.add(lblg2);
-		textFieldAnyoInicio = new JTextField();
 		textFieldAnyoInicio.setEditable(false);
 		textFieldAnyoInicio.setColumns(10);
 		textFieldAnyoInicio.setBounds(359, 220, 66, 20);
@@ -198,7 +188,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblFechaFin.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblFechaFin.setBounds(186, 252, 279, 19);
 		panel.add(lblFechaFin);
-		textFieldDiaFin = new JTextField();
 		textFieldDiaFin.setEditable(false);
 		textFieldDiaFin.setToolTipText("");
 		textFieldDiaFin.setColumns(10);
@@ -209,7 +198,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblg11.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		lblg11.setBounds(253, 279, 16, 14);
 		panel.add(lblg11);
-		textFieldMesFin = new JTextField();
 		textFieldMesFin.setEditable(false);
 		textFieldMesFin.setColumns(10);
 		textFieldMesFin.setBounds(273, 276, 66, 20);
@@ -219,7 +207,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblg21.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		lblg21.setBounds(338, 276, 22, 20);
 		panel.add(lblg21);
-		textFieldAnyoFin = new JTextField();
 		textFieldAnyoFin.setEditable(false);
 		textFieldAnyoFin.setColumns(10);
 		textFieldAnyoFin.setBounds(359, 275, 66, 20);
@@ -229,7 +216,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblTasaMatricula.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblTasaMatricula.setBounds(186, 307, 279, 19);
 		panel.add(lblTasaMatricula);
-		textFieldTasa = new JTextField();
 		textFieldTasa.setEditable(false);
 		textFieldTasa.setBounds(186, 326, 239, 20);
 		panel.add(textFieldTasa);
@@ -239,7 +225,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblEditaLosEcts.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblEditaLosEcts.setBounds(186, 357, 279, 19);
 		panel.add(lblEditaLosEcts);
-		textFieldECTS = new JTextField();
 		textFieldECTS.setEditable(false);
 		textFieldECTS.setColumns(10);
 		textFieldECTS.setBounds(186, 377, 239, 20);
@@ -249,7 +234,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblNombreCentro.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNombreCentro.setBounds(185, 402, 279, 19);
 		panel.add(lblNombreCentro);
-		textNombreCentro = new JTextField();
 		textNombreCentro.setEditable(false);
 		textNombreCentro.setColumns(10);
 		textNombreCentro.setBounds(188, 424, 239, 20);
@@ -259,7 +243,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblDirector.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblDirector.setBounds(184, 452, 279, 19);
 		panel.add(lblDirector);
-		textDirector = new JTextField();
 		textDirector.setEditable(false);
 		textDirector.setColumns(10);
 		textDirector.setBounds(186, 473, 239, 20);
@@ -269,7 +252,6 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblSecretario.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblSecretario.setBounds(181, 497, 279, 19);
 		panel.add(lblSecretario);
-		textFieldSecretario = new JTextField();
 		textFieldSecretario.setEditable(false);
 		textFieldSecretario.setColumns(10);
 		textFieldSecretario.setBounds(186, 516, 239, 20);
@@ -279,164 +261,28 @@ public class PantallaConsultarCursos_Ingresos extends JFrame {
 		lblIngresosTotales.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblIngresosTotales.setBounds(10, 59, 279, 19);
 		panel.add(lblIngresosTotales);
-		txtIngresos = new JTextField();
 		txtIngresos.setBounds(10, 82, 96, 19);
 		panel.add(txtIngresos);
 		txtIngresos.setColumns(10);
 
 		tiposBox.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent itemEvent) {
-				String opcion = tiposBox.getSelectedItem().toString();
-				int ingresosTotales;
+				txtIngresos.setText(null);
+				listModel.removeAllElements();
+				int ingresosTotales = 0;
 
-				int[] idCursos;
-				int[] ingresosCursos;
-				String[] tipoCursos;
+				int[] idCursos = GC.obtenerCursos();
+				int[] ingresosCursos = GC.obtenerIngresos();
+				String[] tipoCursos = GC.obtenerTipoCurso();
 				
-				switch(opcion) {						
-				case "NINGUN CURSO":
-					listModel.removeAllElements();
-					break;
-
-				case "MASTER":
-					txtIngresos.setText(null);
-					listModel.removeAllElements();
-					ingresosTotales=0;
-
-					idCursos = GC.obtenerCursos();
-					ingresosCursos = GC.obtenerIngresos();
-					tipoCursos = GC.obtenerTipoCurso();
-					for(int j=0; j<ingresosCursos.length; j++) {
-						if (tipoCursos[j].equals("MASTER")){
-							listModel.addElement(idCursos[j]);
-							ingresosTotales= ingresosTotales + ingresosCursos[j];
-						}
+				for(int j=0; j<ingresosCursos.length; j++) {
+					if (tipoCursos[j].equals(tiposBox.getSelectedItem().toString())){
+						listModel.addElement(idCursos[j]);
+						ingresosTotales = ingresosTotales + ingresosCursos[j];
 					}
-					txtIngresos.setText(Integer.toString(ingresosTotales));	
-					break;
-					
-				case "EXPERTO":
-					txtIngresos.setText(null);
-					listModel.removeAllElements();
-					ingresosTotales=0;
-
-					idCursos = GC.obtenerCursos();
-					ingresosCursos = GC.obtenerIngresos();
-					tipoCursos = GC.obtenerTipoCurso();
-					for(int j=0; j<ingresosCursos.length; j++) {
-						if (tipoCursos[j].equals("EXPERTO")){
-							listModel.addElement(idCursos[j]);
-							ingresosTotales= ingresosTotales + ingresosCursos[j];
-						}
-					}
-					txtIngresos.setText(Integer.toString(ingresosTotales));	
-					break;
-					
-				case "ESPECIALISTA":
-					txtIngresos.setText(null);
-					listModel.removeAllElements();
-					ingresosTotales=0;
-
-					idCursos = GC.obtenerCursos();
-					ingresosCursos = GC.obtenerIngresos();
-					tipoCursos = GC.obtenerTipoCurso();
-					for(int j=0; j<ingresosCursos.length; j++) {
-						if (tipoCursos[j].equals("ESPECIALISTA")){
-							listModel.addElement(idCursos[j]);
-							ingresosTotales= ingresosTotales + ingresosCursos[j];
-						}
-					}
-					txtIngresos.setText(Integer.toString(ingresosTotales));	
-					break;
-
-				case "FORMACION_AVANZADA":
-					txtIngresos.setText(null);
-					listModel.removeAllElements();
-					ingresosTotales=0;
-
-					idCursos = GC.obtenerCursos();
-					ingresosCursos = GC.obtenerIngresos();
-					tipoCursos = GC.obtenerTipoCurso();
-					for(int j=0; j<ingresosCursos.length; j++) {
-						if (tipoCursos[j].equals("FORMACION_AVANZADA")){
-							listModel.addElement(idCursos[j]);
-							ingresosTotales= ingresosTotales + ingresosCursos[j];
-						}
-					}
-					txtIngresos.setText(Integer.toString(ingresosTotales));	
-					break;
-
-				case "FORMACION_CONTINUA":
-					txtIngresos.setText(null);
-					listModel.removeAllElements();
-					ingresosTotales=0;
-
-					idCursos = GC.obtenerCursos();
-					ingresosCursos = GC.obtenerIngresos();
-					tipoCursos = GC.obtenerTipoCurso();
-					for(int j=0; j<ingresosCursos.length; j++) {
-						if (tipoCursos[j].equals("FORMACION_CONTINUA")){
-							listModel.addElement(idCursos[j]);
-							ingresosTotales= ingresosTotales + ingresosCursos[j];
-						}
-					}
-					txtIngresos.setText(Integer.toString(ingresosTotales));	
-					break;
-
-				case "MICROCREDENCIALES":
-					txtIngresos.setText(null);
-					listModel.removeAllElements();
-					ingresosTotales=0;
-
-					idCursos = GC.obtenerCursos();
-					ingresosCursos = GC.obtenerIngresos();
-					tipoCursos = GC.obtenerTipoCurso();
-					for(int j=0; j<ingresosCursos.length; j++) {
-						if (tipoCursos[j].equals("MICROCREDENCIALES")){
-							listModel.addElement(idCursos[j]);
-							ingresosTotales= ingresosTotales + ingresosCursos[j];
-						}
-					}
-					txtIngresos.setText(Integer.toString(ingresosTotales));	
-					break;	
-
-				case "CORTA_DURACION":
-					txtIngresos.setText(null);
-					listModel.removeAllElements();
-					ingresosTotales=0;
-
-					idCursos = GC.obtenerCursos();
-					ingresosCursos = GC.obtenerIngresos();
-					tipoCursos = GC.obtenerTipoCurso();
-					for(int j=0; j<ingresosCursos.length; j++) {
-						if (tipoCursos[j].equals("CORTA_DURACION")){
-							listModel.addElement(idCursos[j]);
-							ingresosTotales= ingresosTotales + ingresosCursos[j];
-						}
-					}
-					txtIngresos.setText(Integer.toString(ingresosTotales));	
-					break;
-					
-				case "VERANO_Y_EXTENSION":
-					txtIngresos.setText(null);
-					listModel.removeAllElements();
-					ingresosTotales=0;
-
-					idCursos = GC.obtenerCursos();
-					ingresosCursos = GC.obtenerIngresos();
-					tipoCursos = GC.obtenerTipoCurso();
-					for(int j=0; j<ingresosCursos.length; j++) {
-						if (tipoCursos[j].equals("VERANO_Y_EXTENSION")){
-							listModel.addElement(idCursos[j]);
-							ingresosTotales= ingresosTotales + ingresosCursos[j];
-						}
-					}
-					txtIngresos.setText(Integer.toString(ingresosTotales));	
-					break;
-					
-				default:
-					break;
 				}
+				listCursos.updateUI();
+				txtIngresos.setText(Integer.toString(ingresosTotales));
 			}
 		});	
 	}
