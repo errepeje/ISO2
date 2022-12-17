4ab6c502019b01907c99920e08fcd9c46f6d5c19
Fernando <lorentecalvo@gmail.com>
2022-12-17 12:07:46 +0100

    consultaRechazadoAceptado

--- src/main/java/presentacion/PantallaConsultarRechazadoAceptado.java
+++ src/main/java/presentacion/PantallaConsultarRechazadoAceptado.java
@@ -12,6 +12,7 @@ import javax.swing.event.ListSelectionListener;
 import controllers.GestorConsultas;
 import controllers.GestorPropuestasCursos;
 import entities.CursoPropio;
+import entities.EstadoCurso;
 
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -40,8 +41,10 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 	private JTextField textFieldAnyoFin;
 	private JTextField textFieldTasa;
 	private JTextField textFieldECTS;
+	
 	private GestorConsultas GC = new GestorConsultas();
 	private CursoPropio cp;
+	
 	private JTextField textFieldNombre;
 	private JTextField textFieldEdicion;
 	private JTextField textFieldTipoCurso;
@@ -66,9 +69,10 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 	 */
 	@SuppressWarnings("deprecation")
 	public PantallaConsultarRechazadoAceptado() {
+		String fuente = "Tahoma";
 		setTitle("Seleccion cursos estados");
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 728, 597);
+		setBounds(100, 100, 463, 597);
 		panel = new JPanel();
 		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -76,92 +80,81 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 		panel.setLayout(null);
 
 		DefaultListModel listModel = new DefaultListModel();
-
 		JList listCursos = new JList(listModel);
 		listCursos.setBounds(10, 82, 166, 454);
 		panel.add(listCursos);
 
 		JLabel lblFechaInicio = new JLabel("Fecha de inicio (dd-mm-yyyy): ");
-		lblFechaInicio.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblFechaInicio.setFont(new Font(fuente, Font.PLAIN, 15));
 		lblFechaInicio.setBounds(186, 199, 279, 19);
 		panel.add(lblFechaInicio);
-
-		JLabel lblFechaFin = new JLabel("Fecha de fin (dd-mm-yyyy): ");
-		lblFechaFin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblFechaFin.setBounds(186, 252, 279, 19);
-		panel.add(lblFechaFin);
-
-		JLabel lblTasaMatricula = new JLabel("Tasa: ");
-		lblTasaMatricula.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblTasaMatricula.setBounds(186, 307, 279, 19);
-		panel.add(lblTasaMatricula);
-
-		JLabel lblEditaLosEcts = new JLabel("ECTS: ");
-		lblEditaLosEcts.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblEditaLosEcts.setBounds(186, 357, 279, 19);
-		panel.add(lblEditaLosEcts);
-
 		textFieldDiaInicio = new JTextField();
 		textFieldDiaInicio.setEditable(false);
 		textFieldDiaInicio.setToolTipText("");
 		textFieldDiaInicio.setColumns(10);
 		textFieldDiaInicio.setBounds(186, 221, 66, 20);
 		panel.add(textFieldDiaInicio);
-
-		JLabel lblg1 = new JLabel("-");
-		lblg1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblg1.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblg1.setBounds(253, 224, 16, 14);
-		panel.add(lblg1);
-
 		textFieldMesInicio = new JTextField();
 		textFieldMesInicio.setEditable(false);
 		textFieldMesInicio.setColumns(10);
 		textFieldMesInicio.setBounds(273, 221, 66, 20);
 		panel.add(textFieldMesInicio);
-
-		JLabel lblg2 = new JLabel("-");
-		lblg2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblg2.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblg2.setBounds(338, 221, 22, 20);
-		panel.add(lblg2);
-
 		textFieldAnyoInicio = new JTextField();
 		textFieldAnyoInicio.setEditable(false);
 		textFieldAnyoInicio.setColumns(10);
 		textFieldAnyoInicio.setBounds(359, 220, 66, 20);
 		panel.add(textFieldAnyoInicio);
+		JLabel lblg1 = new JLabel("-");
+		lblg1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblg1.setFont(new Font(fuente, Font.PLAIN, 24));
+		lblg1.setBounds(253, 224, 16, 14);
+		panel.add(lblg1);
+		JLabel lblg2 = new JLabel("-");
+		lblg2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblg2.setFont(new Font(fuente, Font.PLAIN, 24));
+		lblg2.setBounds(338, 221, 22, 20);
+		panel.add(lblg2);
 
+		JLabel lblFechaFin = new JLabel("Fecha de fin (dd-mm-yyyy): ");
+		lblFechaFin.setFont(new Font(fuente, Font.PLAIN, 15));
+		lblFechaFin.setBounds(186, 252, 279, 19);
+		panel.add(lblFechaFin);
 		textFieldDiaFin = new JTextField();
 		textFieldDiaFin.setEditable(false);
 		textFieldDiaFin.setToolTipText("");
 		textFieldDiaFin.setColumns(10);
 		textFieldDiaFin.setBounds(186, 276, 66, 20);
 		panel.add(textFieldDiaFin);
-
-		JLabel lblg11 = new JLabel("-");
-		lblg11.setHorizontalAlignment(SwingConstants.CENTER);
-		lblg11.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblg11.setBounds(253, 279, 16, 14);
-		panel.add(lblg11);
-
 		textFieldMesFin = new JTextField();
 		textFieldMesFin.setEditable(false);
 		textFieldMesFin.setColumns(10);
 		textFieldMesFin.setBounds(273, 276, 66, 20);
 		panel.add(textFieldMesFin);
-
-		JLabel lblg21 = new JLabel("-");
-		lblg21.setHorizontalAlignment(SwingConstants.CENTER);
-		lblg21.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblg21.setBounds(338, 276, 22, 20);
-		panel.add(lblg21);
-
 		textFieldAnyoFin = new JTextField();
 		textFieldAnyoFin.setEditable(false);
 		textFieldAnyoFin.setColumns(10);
 		textFieldAnyoFin.setBounds(359, 275, 66, 20);
 		panel.add(textFieldAnyoFin);
+		JLabel lblg11 = new JLabel("-");
+		lblg11.setHorizontalAlignment(SwingConstants.CENTER);
+		lblg11.setFont(new Font(fuente, Font.PLAIN, 24));
+		lblg11.setBounds(253, 279, 16, 14);
+		panel.add(lblg11);
+		JLabel lblg21 = new JLabel("-");
+		lblg21.setHorizontalAlignment(SwingConstants.CENTER);
+		lblg21.setFont(new Font(fuente, Font.PLAIN, 24));
+		lblg21.setBounds(338, 276, 22, 20);
+		panel.add(lblg21);
+
+		JLabel lblTasaMatricula = new JLabel("Tasa: ");
+		lblTasaMatricula.setFont(new Font(fuente, Font.PLAIN, 15));
+		lblTasaMatricula.setBounds(186, 307, 279, 19);
+		panel.add(lblTasaMatricula);
+
+		JLabel lblEditaLosEcts = new JLabel("ECTS: ");
+		lblEditaLosEcts.setFont(new Font(fuente, Font.PLAIN, 15));
+		lblEditaLosEcts.setBounds(186, 357, 279, 19);
+		panel.add(lblEditaLosEcts);
 
 		textFieldTasa = new JTextField();
 		textFieldTasa.setEditable(false);
@@ -176,10 +169,9 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 		panel.add(textFieldECTS);
 
 		JLabel lblNombre = new JLabel("Nombre:");
-		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNombre.setFont(new Font(fuente, Font.PLAIN, 15));
 		lblNombre.setBounds(186, 11, 279, 19);
 		panel.add(lblNombre);
-
 		textFieldNombre = new JTextField();
 		textFieldNombre.setToolTipText("");
 		textFieldNombre.setEditable(false);
@@ -195,12 +187,12 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 		panel.add(textFieldEdicion);
 
 		JLabel lblEdicion = new JLabel("Edicion:");
-		lblEdicion.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblEdicion.setFont(new Font(fuente, Font.PLAIN, 15));
 		lblEdicion.setBounds(186, 60, 279, 19);
 		panel.add(lblEdicion);
 
 		JLabel lblTipoDeCurso = new JLabel("Tipo de curso:");
-		lblTipoDeCurso.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblTipoDeCurso.setFont(new Font(fuente, Font.PLAIN, 15));
 		lblTipoDeCurso.setBounds(186, 115, 279, 19);
 		panel.add(lblTipoDeCurso);
 
@@ -212,10 +204,9 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 		panel.add(textFieldTipoCurso);
 
 		JLabel lblEstadoCurso = new JLabel("Estado del curso:");
-		lblEstadoCurso.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblEstadoCurso.setFont(new Font(fuente, Font.PLAIN, 15));
 		lblEstadoCurso.setBounds(185, 160, 279, 19);
 		panel.add(lblEstadoCurso);
-
 		textFieldEstadoCurso = new JTextField();
 		textFieldEstadoCurso.setToolTipText("");
 		textFieldEstadoCurso.setEditable(false);
@@ -224,10 +215,9 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 		panel.add(textFieldEstadoCurso);
 
 		JLabel lblNombreCentro = new JLabel("Nombre del centro:");
-		lblNombreCentro.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNombreCentro.setFont(new Font(fuente, Font.PLAIN, 15));
 		lblNombreCentro.setBounds(185, 402, 279, 19);
 		panel.add(lblNombreCentro);
-
 		textNombreCentro = new JTextField();
 		textNombreCentro.setEditable(false);
 		textNombreCentro.setColumns(10);
@@ -235,10 +225,9 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 		panel.add(textNombreCentro);
 
 		JLabel lblDirector = new JLabel("Director:");
-		lblDirector.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblDirector.setFont(new Font(fuente, Font.PLAIN, 15));
 		lblDirector.setBounds(184, 452, 279, 19);
 		panel.add(lblDirector);
-
 		textDirector = new JTextField();
 		textDirector.setEditable(false);
 		textDirector.setColumns(10);
@@ -246,10 +235,9 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 		panel.add(textDirector);
 
 		JLabel lblSecretario = new JLabel("Secretario:");
-		lblSecretario.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblSecretario.setFont(new Font(fuente, Font.PLAIN, 15));
 		lblSecretario.setBounds(181, 497, 279, 19);
 		panel.add(lblSecretario);
-
 		textFieldSecretario = new JTextField();
 		textFieldSecretario.setEditable(false);
 		textFieldSecretario.setColumns(10);
@@ -257,104 +245,27 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 		panel.add(textFieldSecretario);
 		textFieldTasa.setEditable(false);
 
-
-
-
-		JComboBox estadosBox = new JComboBox();
+		JComboBox estadosBox = new JComboBox(EstadoCurso.values());
 		estadosBox.setBounds(10, 39, 166, 21);
 		panel.add(estadosBox);
-		String [] allEstados = {"NINGUN CURSO","PROPUESTO", "VALIDADO", "PROPUESTA_RECHAZADA", "EN_MATRICULACIÓN", "EN_IMPARTICIÓN", "TERMINADO"};
-		for(int i=0; i<allEstados.length; i++)
-			estadosBox.addItem(allEstados[i]);
-
-		JLabel lblEstadoDeCurso = new JLabel("Estado de curso:");
-		lblEstadoDeCurso.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblEstadoDeCurso.setBounds(10, 11, 279, 19);
-		panel.add(lblEstadoDeCurso);
-
-		/* ACCIONES */
 		estadosBox.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent itemEvent) {
-				String opcion = estadosBox.getSelectedItem().toString();
+				listModel.removeAllElements();
 				int[] idCursos = GC.obtenerCursos();
-				String[] estadoCursos;
+				String[] estadoCursos = GC.obtenerEstado();
 				
-				switch(opcion) {						
-				case "NINGUN CURSO":
-					listModel.removeAllElements();
-					break;
-
-				case "PROPUESTO":
-					listModel.removeAllElements();
-					idCursos = GC.obtenerCursos();
-					estadoCursos = GC.obtenerEstado();
-					for(int j=0; j<estadoCursos.length; j++) {
-						if (estadoCursos[j].equals("PROPUESTO")){
-							listModel.addElement(idCursos[j]);
-						}
+				for(int j=0; j<estadoCursos.length; j++) {
+					if (estadoCursos[j].equals(estadosBox.getSelectedItem().toString())) {
+						listModel.addElement(idCursos[j]);
 					}
-					break;
-					
-				case "VALIDADO":
-					listModel.removeAllElements();
-					idCursos = GC.obtenerCursos();
-					estadoCursos = GC.obtenerEstado();
-					for(int j=0; j<estadoCursos.length; j++) {
-						if (estadoCursos[j].equals("VALIDADO")){
-							listModel.addElement(idCursos[j]);
-						}
-					}
-					break;
-					
-				case "PROPUESTA_RECHAZADA":
-					listModel.removeAllElements();
-					idCursos = GC.obtenerCursos();
-					estadoCursos = GC.obtenerEstado();
-					for(int j=0; j<estadoCursos.length; j++) {
-						if (estadoCursos[j].equals("PROPUESTA_RECHAZADA")){
-							listModel.addElement(idCursos[j]);
-						}
-					}
-					break;
-
-				case "EN_MATRICULACIÓN":
-					listModel.removeAllElements();
-					idCursos = GC.obtenerCursos();
-					estadoCursos = GC.obtenerEstado();
-					for(int j=0; j<estadoCursos.length; j++) {
-						if (estadoCursos[j].equals("EN_MATRICULACIÓN")){
-							listModel.addElement(idCursos[j]);
-						}
-					}
-					break;
-
-				case "EN_IMPARTICIÓN":
-					listModel.removeAllElements();
-					idCursos = GC.obtenerCursos();
-					estadoCursos = GC.obtenerEstado();
-					for(int j=0; j<estadoCursos.length; j++) {
-						if (estadoCursos[j].equals("EN_IMPARTICIÓN")){
-							listModel.addElement(idCursos[j]);
-						}
-					}
-					break;
-
-				case "TERMINADO":
-					listModel.removeAllElements();
-					idCursos = GC.obtenerCursos();
-					estadoCursos = GC.obtenerEstado();
-					for(int j=0; j<estadoCursos.length; j++) {
-						if (estadoCursos[j].equals("TERMINADO")){
-							listModel.addElement(idCursos[j]);
-						}
-					}
-					break;
-					
-				default:
-					break;
 				}
 			}
 		});
+
+		JLabel lblEstadoDeCurso = new JLabel("Estado de curso:");
+		lblEstadoDeCurso.setFont(new Font(fuente, Font.PLAIN, 15));
+		lblEstadoDeCurso.setBounds(10, 11, 279, 19);
+		panel.add(lblEstadoDeCurso);
 
 		listCursos.addListSelectionListener(new ListSelectionListener() {
 			@Override
@@ -364,11 +275,6 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 					textFieldTasa.setText(Integer.toString(cp.gettasaMatricula()));
 					textFieldECTS.setText(Integer.toString(cp.geteCTS()));
 
-					String[] resultInicio = cp.getfechaInicio().toString().split("-");
-					String[] solI = resultInicio[2].split(" ");
-					String[] resultFin = cp.getfechaFin().toString().split("-");
-					String[] solF = resultFin[2].split(" ");
-
 					textFieldNombre.setText(cp.getnombre());
 					textFieldEdicion.setText(Integer.toString(cp.getedicion()));
 					textNombreCentro.setText(cp.getNombreCentro());
@@ -377,10 +283,14 @@ public class PantallaConsultarRechazadoAceptado extends JFrame {
 					textFieldTipoCurso.setText(cp.gettipo().toString());
 					textFieldEstadoCurso.setText(cp.getestado().toString());
 
+					String[] resultInicio = cp.getfechaInicio().toString().split("-");
+					String[] solI = resultInicio[2].split(" ");
 					textFieldDiaInicio.setText(solI[0]);
 					textFieldMesInicio.setText(Integer.toString(cp.getfechaInicio().getMonth() + 1));
 					textFieldAnyoInicio.setText(Integer.toString(cp.getfechaInicio().getYear() + 1900));
 
+					String[] resultFin = cp.getfechaFin().toString().split("-");
+					String[] solF = resultFin[2].split(" ");
 					textFieldDiaFin.setText(solF[0]);
 					textFieldMesFin.setText(Integer.toString(cp.getfechaFin().getMonth() + 1));
 					textFieldAnyoFin.setText(Integer.toString(cp.getfechaFin().getYear() + 1900));
